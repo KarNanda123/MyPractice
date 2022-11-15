0511c3dcf164dbf164ed1b491874ea7afa483c46
LENOVO <LENOVO@DESKTOP-6F1UH3N>
2022-11-15 11:22:15 +0530

    I am commiting this code

---
+++ ghk/src/test/java/pack/work.java
@@ -0,0 +1,16 @@
+package pack;
+
+public class work {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("class1");
+		System.out.println("class1");
+		System.out.println("class1");
+		System.out.println("class1");
+		System.out.println("class1");
+		System.out.println("class1");
+		System.out.println("class1");
+		
+	}
+}
