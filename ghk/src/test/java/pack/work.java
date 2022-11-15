2c020ec9c642caddd505583d8f2473d04389fe50
KarNanda123 <118243349+KarNanda123@users.noreply.github.com>
2022-11-15 12:05:20 +0530

    Update work.java

--- ghk/src/test/java/pack/work.java
+++ ghk/src/test/java/pack/work.java
@@ -5,12 +5,6 @@ public class work {
 	public static void main(String[] args) {
 		
 		System.out.println("class1");
-		System.out.println("class1");
-		System.out.println("class1");
-		System.out.println("class1");
-		System.out.println("class1");
-		System.out.println("class1");
-		System.out.println("class1");
 		
 	}
 }
